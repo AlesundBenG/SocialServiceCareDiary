74e8a82a2dc0682787cacbcdc7c2ab251f7c73ee
Паша <77670636+AlesundBenG@users.noreply.github.com>
2023-08-16 10:12:37 +0300

    Таблица CARE_DIARY_PLAN_ON_DAY
    Убраны избыточные поля в таблице ежедневного плана (данные поля дублировали данные из таблицы ежемесячного отчета).

--- Скрипты/3. Генерация ежедневного плана оказания услуг в дневнике ухода.sql
+++ Скрипты/3. Генерация ежедневного плана оказания услуг в дневнике ухода.sql
@@ -20,16 +20,12 @@ IF OBJECT_ID('tempdb..#WORK_PLAN')          IS NOT NULL BEGIN DROP TABLE #WORK_P
 CREATE TABLE #SERV_SDU_FOR_DAY (
     CARE_DIARY_OUID     INT,            --Идентификатор дневника ухода.
     CARE_DIARY_REPORT   INT,            --Идентификатор ежемесячного отчета, по которому создается план.
-    SERV_SDU            INT,            --Услуга.
-    EXECUTE_TIME        VARCHAR(32),    --Время для исполнения услуги (в минутах).
     COUNT_IN_DAY        INT,            --Количество услуг в день.
 
 )
 CREATE TABLE #WORK_PLAN (
     CARE_DIARY_OUID     INT,    --Идентификатор дневника ухода.
     CARE_DIARY_REPORT   INT,    --Идентификатор ежемесячного отчета, по которому создается план.
-    SERV_SDU            INT,    --Услуга.
-    EXECUTE_TIME        INT,    --Необходимое время на выполнение.
     COUNT_IN_DAY        INT,    --Количество услуг в день.
     INDEX_IN_DAY        INT,    --Индекс услуги.
     DATE                DATE,   --Дата, для которой сформирована услуга.
@@ -42,12 +38,10 @@ CREATE TABLE #WORK_PLAN (
 --Формирование планов на день. 
 DECLARE @query NVARCHAR(MAX) = ''
 SET @query = '
-    INSERT INTO #SERV_SDU_FOR_DAY(CARE_DIARY_OUID, CARE_DIARY_REPORT, SERV_SDU, EXECUTE_TIME, COUNT_IN_DAY)
+    INSERT INTO #SERV_SDU_FOR_DAY(CARE_DIARY_OUID, CARE_DIARY_REPORT, COUNT_IN_DAY)
     SELECT
         report.CARE_DIARY_OUID                          AS CARE_DIARY_OUID,
         report.A_OUID                                   AS CARE_DIARY_REPORT,
-        report.SERV_SDU                                 AS SERV_SDU,
-        report.EXECUTE_TIME                             AS EXECUTE_TIME,
         report.DAY_' +  CONVERT(VARCHAR, @day) + '_ALL  AS COUNT_IN_DAY
     FROM CARE_DIARY_REPORT report --План-отчет предоставления услуг СДУ из дневника ухода.
     ----План предоставления услуг на дату.
@@ -72,26 +66,24 @@ EXEC SP_EXECUTESQL @query
 --Дублирование услуг в соответствии с количеством оказанием на дню.
 DECLARE @careDiary          INT         --Идентификатор дневника ухода.
 DECLARE @careDiaryReport    INT         --Идентификатор ежемесячного отчета.
-DECLARE @servSDU            INT         --Услуга.
-DECLARE @executeTime        VARCHAR(32) --Время для исполнения услуги.
 DECLARE @countInDay         INT         --Количество услуг в день.
 DECLARE @cursorMultiplyRows CURSOR      --Курсор для приумножения строк в соответствии со значением.
 
 --Формирования xml-текста документов.                         
 SET @cursorMultiplyRows = CURSOR SCROLL FOR --Заполнение курсора.
-    SELECT CARE_DIARY_OUID, CARE_DIARY_REPORT, SERV_SDU, EXECUTE_TIME, COUNT_IN_DAY FROM #SERV_SDU_FOR_DAY  
+    SELECT CARE_DIARY_OUID, CARE_DIARY_REPORT, COUNT_IN_DAY FROM #SERV_SDU_FOR_DAY  
 OPEN @cursorMultiplyRows --Открытие курсора.
-FETCH NEXT FROM @cursorMultiplyRows INTO @careDiary, @careDiaryReport, @servSDU, @executeTime, @countInDay --Выбор первой записи.
+FETCH NEXT FROM @cursorMultiplyRows INTO @careDiary, @careDiaryReport, @countInDay --Выбор первой записи.
 --Основной цикл курсора.
 WHILE @@FETCH_STATUS = 0 BEGIN
     DECLARE @count INT = 0
     WHILE @count < @countInDay BEGIN
-        INSERT INTO #WORK_PLAN (CARE_DIARY_OUID, CARE_DIARY_REPORT, SERV_SDU, EXECUTE_TIME, COUNT_IN_DAY, INDEX_IN_DAY, DATE, WEEK_NUMBER, DAY_NUMBER)
-        VALUES(@careDiary, @careDiaryReport, @servSDU, @executeTime, @countInDay, @count + 1, @dateForPlan, @weekForPlan, @dayForPlan)
+        INSERT INTO #WORK_PLAN (CARE_DIARY_OUID, CARE_DIARY_REPORT, COUNT_IN_DAY, INDEX_IN_DAY, DATE, WEEK_NUMBER, DAY_NUMBER)
+        VALUES(@careDiary, @careDiaryReport, @countInDay, @count + 1, @dateForPlan, @weekForPlan, @dayForPlan)
         SET @count = @count + 1 
     END
      --Выбираем данные изе первой строки курсора
-    FETCH NEXT FROM @cursorMultiplyRows INTO @careDiary, @careDiaryReport, @servSDU, @executeTime, @countInDay
+    FETCH NEXT FROM @cursorMultiplyRows INTO @careDiary, @careDiaryReport, @countInDay
 END
 CLOSE @cursorMultiplyRows --Закрытие курсора.
 DEALLOCATE @cursorMultiplyRows --Освобождения ресурсов, выделенные курсору.     
@@ -99,12 +91,10 @@ DEALLOCATE @cursorMultiplyRows --Освобождения ресурсов, вы
 ------------------------------------------------------------------------------------------------------------------------------
 
 --Вставка плана.
-INSERT INTO CARE_DIARY_PLAN_ON_DAY(CARE_DIARY_OUID, CARE_DIARY_REPORT, SERV_SDU, EXECUTE_TIME, INDEX_IN_DAY, COUNT_IN_DAY, DATE, WEEK_NUMBER, DAY_NUMBER, PERFORM, A_STATUS, A_CREATEDATE, CURRENTLY_EDITED)
+INSERT INTO CARE_DIARY_PLAN_ON_DAY(CARE_DIARY_OUID, CARE_DIARY_REPORT, INDEX_IN_DAY, COUNT_IN_DAY, DATE, WEEK_NUMBER, DAY_NUMBER, PERFORM, A_STATUS, A_CREATEDATE)
 SELECT 
     CARE_DIARY_OUID     AS CARE_DIARY_OUID, 
     CARE_DIARY_REPORT   AS CARE_DIARY_REPORT,
-    SERV_SDU            AS SERV_SDU, 
-    EXECUTE_TIME        AS EXECUTE_TIME, 
     INDEX_IN_DAY        AS INDEX_IN_DAY, 
     COUNT_IN_DAY        AS COUNT_IN_DAY, 
     DATE                AS DATE, 
@@ -112,8 +102,7 @@ SELECT
     DAY_NUMBER          AS DAY_NUMBER, 
     0                   AS PERFORM,
     10                  AS A_STATUS,
-    GETDATE()           AS A_CREATEDATE,
-    1                   AS CURRENTLY_EDITED
+    GETDATE()           AS A_CREATEDATE
 FROM #WORK_PLAN
 
 ------------------------------------------------------------------------------------------------------------------------------
